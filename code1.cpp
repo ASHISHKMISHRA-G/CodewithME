a61e629a5645280449d6fe6542adffd2d3b12181
ASHISHKMISHRA-G <112560490+ASHISHKMISHRA-G@users.noreply.github.com>
2022-08-31 19:25:06 +0530

    Add files via upload

---
+++ code1.cpp
@@ -0,0 +1,25 @@
+/*
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Hello World";
+
+    return 0;
+}
+
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int x;
+    cout<<"Input X= ";
+    cin>>x;
+
+    cout<<x;
+}
