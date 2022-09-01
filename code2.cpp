e833de66b2ed7ea1dffe66777eabc34afa450fbc
ASHISHKMISHRA-G <112560490+ASHISHKMISHRA-G@users.noreply.github.com>
2022-09-01 22:10:38 +0530

    Add files via upload

---
+++ code2.cpp
@@ -0,0 +1,20 @@
+// programs for Use of operators
+
+// Length and breadth of a rectangle are 5 and 7 respectively. Write a program to calculate the area and perimeter of the rectangle
+#include<iostream>
+using namespace std;
+
+/*
+int main(){
+    int l=5;
+    int b=7;
+    int area=0;  // l= lenght , b= breadth
+
+    area=l*b;
+    cout<<"Area of the Rectangle= "<<area;
+
+    return 0;
+    }
+
+*/
+
