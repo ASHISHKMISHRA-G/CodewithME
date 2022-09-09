78ed5094471bc8d40103cd8b3374d873965f0f00
unknown <you@example.com>
2022-09-09 19:21:00 +0530

    Operator4.cpp

--- Operator4.cpp
+++ Operator4.cpp
@@ -1,51 +1,3 @@
-<<<<<<< HEAD
-//C++ Program to Swap Two Numbers.
-
-
-#include<iostream>
-using namespace std;
-
-
-int main(){
-    int x=3,y=7,temp;
-
-    cout<<"Before Swapping:- ";
-    cout<<"\nx= "<<x;
-    cout<<"\ny= "<<y;
-
-    temp=x;
-    x=y;
-    y=temp;
-
-    cout<<"\nAfter Swapping:- ";
-    cout<<"\nx= "<<x;
-    cout<<"\ny= "<<y;
-
-    return 0;
-
-}
-
-
-/*
-int main(){
-    int x=3,y=7;
-
-    cout<<"Before Swapping:- ";
-    cout<<"\nx= "<<x;
-    cout<<"\ny= "<<y;
-
-    x=x+y;
-    y=x-y;
-    x=x-y;
-
-    cout<<"\nAfter Swapping:- ";
-    cout<<"\nx= "<<x;
-    cout<<"\ny= "<<y;
-
-    return 0;
-}
-*/
-=======
 //C++ Program to Swap Two Numbers.
 
 
@@ -92,4 +44,3 @@ int main(){
     return 0;
 }
 */
->>>>>>> 4324dd994ff7b083d5da4c53cbb0a91330e140cf
