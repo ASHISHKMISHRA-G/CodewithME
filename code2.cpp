ff7da58577f08034c0730437a8b9249476936deb
unknown <you@example.com>
2022-09-09 16:46:57 +0530

    code2.cpp

--- code2.cpp
+++ code2.cpp
@@ -9,12 +9,15 @@ int main(){
     int l=5;
     int b=7;
     int area=0;  // l= lenght , b= breadth
+    int perimeter=0;
 
     area=l*b;
+    perimeter=2*(l+b);
+
     cout<<"Rectangle measurements";
     cout<<"\nArea of the Rectangle= "<<area;
-    cout<<"\n!!Thank You!!";
-
+    cout<<"\nPerimeter of the Rectangle= "<<perimeter;
+    
     return 0;
     }
 
