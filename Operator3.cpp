26d95732582c105e4dbc1594ba4ba6eb41e92445
unknown <you@example.com>
2022-09-09 18:02:21 +0530

    Operator3.cpp

---
+++ Operator3.cpp
@@ -0,0 +1,15 @@
+//Write a program to enter a 4 digit number from the keyboard. Add 8 to the number and then divide it by 3. Now, 
+//the modulus of that number is taken with 5 and then multiply the resultant value by 5. Display the final result.
+#include<iostream>
+using namespace stdl;
+
+int main(){
+    int x,result=0;
+    cout<<"Enter a 4 digit no.= ";
+    cin>>x;
+
+    result=(((x+8)/3)%5)*5;
+    cout<<"Result= "<<result;
+
+    return 0 ;
+
