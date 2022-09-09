99d61f949e3590403e6440d6984f88ba84eb9600
unknown <you@example.com>
2022-09-09 19:19:02 +0530

    Operator4.cpp

--- Operator3.cpp
+++ Operator3.cpp
@@ -1,7 +1,7 @@
 //Write a program to enter a 4 digit number from the keyboard. Add 8 to the number and then divide it by 3. Now, 
 //the modulus of that number is taken with 5 and then multiply the resultant value by 5. Display the final result.
 #include<iostream>
-using namespace stdl;
+using namespace std;
 
 int main(){
     int x,result=0;
---
+++ Operator4.cpp
@@ -0,0 +1,46 @@
+//C++ Program to Swap Two Numbers.
+
+
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    int x=3,y=7,temp;
+
+    cout<<"Before Swapping:- ";
+    cout<<"\nx= "<<x;
+    cout<<"\ny= "<<y;
+
+    temp=x;
+    x=y;
+    y=temp;
+
+    cout<<"\nAfter Swapping:- ";
+    cout<<"\nx= "<<x;
+    cout<<"\ny= "<<y;
+
+    return 0;
+
+}
+
+
+/*
+int main(){
+    int x=3,y=7;
+
+    cout<<"Before Swapping:- ";
+    cout<<"\nx= "<<x;
+    cout<<"\ny= "<<y;
+
+    x=x+y;
+    y=x-y;
+    x=x-y;
+
+    cout<<"\nAfter Swapping:- ";
+    cout<<"\nx= "<<x;
+    cout<<"\ny= "<<y;
+
+    return 0;
+}
+*/
