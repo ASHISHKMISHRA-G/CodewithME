5c539dc9db18f0e33ebace40c871fefda7994c10
unknown <you@example.com>
2022-09-14 18:00:22 +0530

    If_else7.cpp

---
+++ If_else6.cpp
@@ -0,0 +1,122 @@
+// Take input of age of 3 people by user and determine oldest and youngest among them.
+
+/*
+#include<iostream>
+using namespace std;
+ int main(){
+    int age1,age2,age3;
+
+    cout<<"Enter age1= ";
+    cin>>age1;
+    cout<<"Enter age2= ";
+    cin>>age2;
+    cout<<"Enter age3= ";
+    cin>>age3;
+
+    if((age1>age2) && (age1>age3)){
+        cout<<"age1 is greatest";
+    }
+    else{
+        cout<<"age1 is smallest";
+    }
+
+    if((age2>age1) && (age2>age3)){
+        cout<<"age2 is greatest";
+    }
+    else{
+        cout<<"age2 is smallest";
+    }
+    
+    if((age3>age1) && (age3>age2)){
+        cout<<"age3 is greatest";
+    }
+    else{
+        cout<<"age3 is smallest";
+    }
+    
+    return 0;
+ }
+*/
+ 
+
+//  #include <iostream>
+
+// using namespace std;
+
+// int main(){
+
+// int r,a,y;
+
+// cout<<"Enter age of the 1st person:";
+
+// cin>> r; 
+
+// cout<<"Enter age of the 2nd person:";
+
+// cin >> a;
+
+// cout <<"Enter age of the 3rd person:";
+
+// cin >> y;
+
+
+
+// if((r>a)&&(r>y)){
+
+//      cout <<"1st person is the oldest \n";
+
+//          if(a<y){
+
+//                   cout <<"2nd person is the youngest \n";
+
+//              }
+
+//         else{
+
+//                  cout<<"3rd person is the youngest \n";  
+
+//           }
+
+// }
+
+// if((a>r)&&(a>y)){
+
+//      cout <<"2nd person is the oldest \n";
+
+//        if(r<y){
+
+//             cout <<"1st person is the youngest \n";
+
+//            }
+
+//        else{
+
+//           cout<<"3rd person is the youngest \n";      
+
+//            }
+
+// }
+
+// else{
+
+//      cout <<"3rd person is the oldest \n";
+
+//          if(r<a){
+
+//            cout << "the 1st person is the youngest" ;
+
+//            }
+
+//        else{
+
+//            cout<<"the 2nd person is the youngest \n";
+
+//           }
+
+// }
+
+
+
+// return 0;
+
+// }
