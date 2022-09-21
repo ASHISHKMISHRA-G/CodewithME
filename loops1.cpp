51ce71850982410098474e20624d7deb1a7b408c
unknown <you@example.com>
2022-09-21 16:59:37 +0530

    loops1.cpp

---
+++ loops1.cpp
@@ -0,0 +1,11 @@
+// Print multiplication table of 24, 50 and 29 using loop.
+#include<iostream>
+using namespace std;
+
+int main(){
+    for(int i=0;i<=10;i++){
+        cout<<"24 X "<<i<<" = "<<24*i<<"\t\t50 X "<<i<<" = "<<50*i<<"\t\t29 X "<<i<<" = "<<29*i<<endl;
+    }
+
+    return 0;
+}
